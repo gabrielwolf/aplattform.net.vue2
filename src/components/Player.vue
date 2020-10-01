ba37a436cea518234c72895f516ed4fb92b0c04c
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2020-10-01 10:44:34 +0200

    WIP

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -10,6 +10,16 @@
 <script>
 import { mapGetters, mapMutations } from 'vuex'
 
+const state = Vue.observable({ trackMeta: 0 })
+
+const trackMeta = {
+  render(h) {
+    return h('button', {
+      on: { click: () => { state.trackMeta += 1 } },
+    }, `trackMeta is: ${state.trackMeta}`)
+  },
+}
+
 export default {
   computed: {
     ...mapGetters('player', ['trackMeta']),
